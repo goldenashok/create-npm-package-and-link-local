1c7ad49c7f5ef5bab17d20e60c907b770ca7a8f4
Ashok Kumar D <goldenashok@users.noreply.github.com>
2022-11-26 20:26:56 +0530

    Add files via upload

---
+++ my-app/index.js
@@ -0,0 +1,4 @@
+const {multiplication, add} = require('my-package');
+
+console.log('Ashok ', multiplication(5,2));
+console.log('Ashok', add(5, 5));
---
+++ my-package/index.js
@@ -0,0 +1,11 @@
+console.log('Own NPM Package');
+const multiplication = (x, y) => {
+    return x * y;
+}
+
+const add = (x, y) => {
+    return x+y;
+}
+
+
+module.exports =  {multiplication, add};
